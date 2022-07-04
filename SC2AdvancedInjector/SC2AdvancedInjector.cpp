51a79637abbe9cecdbc09429a7aaa7e9f0610917
mlcoding111 <kennycodes@hotmail.com>
2022-07-03 23:32:01 -0400

    Mouse positions now overwrite the location vector when setting the positions

--- SC2AdvancedInjector/SC2AdvancedInjector.cpp
+++ SC2AdvancedInjector/SC2AdvancedInjector.cpp
@@ -21,23 +21,35 @@ int main()
     // i.MinimapInject();
 
     setPositions();
+
+
+    return 0;
 }
 
 void setPositions() {
+    i.xLocation.clear();
+    i.yLocation.clear();
+
     std::cout << "Please Proceed. Press X when you're done .." << std::endl;
     while (1) {
         // 0x58 = x key
         if (GetAsyncKeyState(0x58))
         {
-            printf("X Arrow key is Pressed");
+
             break;
         }
         if (GetAsyncKeyState(VK_SPACE))
         {
-            i.xLocation.clear();
-            i.yLocation.clear();
 
-            printf("Saving location");
+            POINT p;
+
+            if (GetCursorPos(&p)) {
+                i.xLocation.push_back(p.x);
+                i.yLocation.push_back(p.y);
+                Beep(750, 500);
+                std::cout << "Location saved successfully" << std::endl;
+                std::cout << "Current vector size : " << i.xLocation.size() << std::endl;
+            }
             
         }
         Sleep(10);
